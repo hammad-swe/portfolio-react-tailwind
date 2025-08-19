e8da9ca0d4039b356ae00e6d8da46434ea9248b0
Hammad Ali <hammadali78056@gmail.com>
2025-08-19 19:07:44 +0500

    feat: designing home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div className="mx-auto w-full max-w-7xl">
-            <h1 className="text-4xl font-bold text-center mt-10">Welcome to My Portfolio</h1>
+        <div className="mx-auto w-full max-w-9xl 
+        bg-[url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]" >
+            <h1 className="text-4xl font-bold text-center p-83">Welcome to My Portfolio</h1>
         </div>
     );
 }
