e4f0e70d960f5354a7e3881a9c045c852b6ea8d6
Hammad Ali <hammadali78056@gmail.com>
2025-08-22 11:10:38 +0500

    fix:bug in contact button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -53,15 +53,12 @@ import { useState } from "react"
 
 export default () => {
 
-    const [state, setState] = useState(false)
+    const [state, setState] = useState(false);
+    const navigate = useNavigate();
 
-    // Replace javascript:void(0) path with your path
-    const navigation = [
-        { title: "contact", path: "/contact" },
-        { title: "Customers", path: "javascript:void(0)" },
-        { title: "Team", path: "javascript:void(0)" },
-
-    ]
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
 
     return (
         <>
@@ -81,7 +78,7 @@ export default () => {
                             <a href="/contact" className="block py-2 px-4 text-center text-white font-medium bg-blue-500 duration-150 hover:bg-blue-400 active:bg-blue-600 rounded-lg shadow-lg hover:shadow-none">
                                 Contact Me
                             </a>
-                            <a href="javascript:void(0)" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
+                            <a href="/contact" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
                                 Download CV
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
